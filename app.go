b51b44d37d44fe0facc0f2f0cd7dcdf6147d8d3d
Dominique Luna <dluna132@gmail.com>
2014-05-09 15:28:42 -0400

    first

---
+++ app.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/render"
+)
+
+func main() {
+	m := martini.Classic()
+    m.Use(render.Renderer(render.Options{
+        Layout: "layout",
+    }))
+
+	m.Get("/", func(r render.Render) {
+		r.HTML(200, "baseball", nil)
+	})
+	m.Run()
+}
