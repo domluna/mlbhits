206c771c29b78d5ba0c5e284cc4c7c3516c18f86
Dominique Luna <dluna132@gmail.com>
2014-05-09 16:38:24 -0400

    Added initial data, gonna get the data for the whole 2013 year

--- public/js/baseball.js
+++ public/js/baseball.js
@@ -1,1 +1,18 @@
+(function(d3, JQuery) {
+  d3.select('body').append('p').text('New paragraph');
+  d3.csv('data/baseball-pitches-clean.csv')
+  .row(function(d) {
+    return {
+      pitchName: d.pitch_name,
+      startSpeed: d.start_speed
+    };
+  })
+  .get(function(err, rows) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(rows);
+  })
 
+})(d3, $);
